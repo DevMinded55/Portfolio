addde330db4730e944669a4eabbcc792de313cd2
DevMinded <keles490@googlemail.com>
2025-06-27 04:21:45 +0200

    included HTML + SCSS but styling not finished yet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { FooterComponent } from './footer/footer.component';
+import { ReferencesComponent } from './references/references.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,AboveTheFoldComponent,AboutMeComponent,MySkillsComponent,PortfolioComponent,FooterComponent, ReferencesComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { FooterComponent } from './footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //{path: "", component: HeaderComponent},
+    //{path: "", component: AboveTheFoldComponent},
+    //{path: "", component: AboutMeComponent},
+    //{path: "", component: MySkillsComponent},
+    //{path: "", component: PortfolioComponent},
+    //{path: "", component: FooterComponent},
+];
